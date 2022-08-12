1d951cd10f9c884feb2e85e2890409485fe220e3
Vitor Macedo <vitorjvbm@gmail.com>
2022-08-12 04:53:48 -0300

    feat: login auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,6 @@
 import authRepository from "../repositories/authRepository.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export async function signUp(req, res) {
     const user = req.body;
@@ -15,5 +17,34 @@ export async function signUp(req, res) {
     }catch(error){
         console.log(error);
         return res.status(500).send("Server Error!");
+    }
+}
+
+export async function login(req, res) {
+    const{ email, password } = req.body;
+
+    try{
+        const user = await authRepository.getUserByEmail(email)
+        if(user.rowCount <= 0){
+            return res.status(401).send("Unauthorized");
+        }
+        if(bcrypt.compareSync(password, user.rows[0].password)){
+            const data = {
+                userId: user.rows[0].id
+            }
+            const token = jwt.sign(data, process.env.JWT_KEY, { expiresIn: '1h' })
+            return res.status(200).send({
+                token: token,
+                userId: user.rows[0].id,
+                username: user.rows[0].username,
+                pictureUrl: user.rows[0].pictureUrl
+            })
+        }
+
+        return res.status(401).send("Unauthorized");
+
+    }catch(error){
+        console.log(error);
+        return res.sendStatus(500);
     }
 }
--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { signUp } from "../controllers/authController.js";
+import { login, signUp } from "../controllers/authController.js";
 import { schemaValidator } from "../middlewares/schemaValidator.js";
+import loginSchema from "../schemas/loginSchema.js";
 import userSchema from "../schemas/userSchema.js";
 
 const authRouter = Router();
 
-// login
-//  authRouter.post("/", schemaValidator(loginSchema), login)
+authRouter.post("/", schemaValidator(loginSchema), login)
 authRouter.post("/sign-up", schemaValidator(userSchema), signUp)
 
 export default authRouter; 
